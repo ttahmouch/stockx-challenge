0808b90410d679e5a6e315de120640bb4f8a2d17
Tony Tahmouch <tony.tahmouch@gm.com>
2019-02-14 10:04:07 -0500

    TT | Replacing the usage of the in-memory database with queries to the actual Postgres database container to retrieve Shoe resource data.

--- params/index.js
+++ params/index.js
@@ -1,24 +1,27 @@
-const TEMPORARY_SHOE_DATABASE = {
-    'adidas Yeezy': {
-        'name': 'adidas Yeezy',
-        'true_to_sizes': [],
-        'true_to_size_calculation': 0
-    }
-};
-
 module.exports = {
     onShoeResourceRequested: (req, res, next, shoe) => {
-        let entry = TEMPORARY_SHOE_DATABASE[shoe];
+        let retrieveAllShoeResourcesWithName = `SELECT * FROM shoes WHERE name='${shoe}';`;
+        let retrieveTrueToSizeDataForShoeResource = `SELECT * FROM shoes_true_to_size_data WHERE name='${shoe}';`;
 
-        if (!entry) {
-            return res.locals.sendResponse({
-                status: 404,
-                headers: {},
-                body: {reason: 'Not Found'}
-            });
-        } else {
-            res.locals.shoe = entry;
-            return next();
-        }
+        return res.locals.database.query(retrieveAllShoeResourcesWithName, (error, {rowCount}) => {
+            if (error) {
+                return next(error);
+            } else if (!rowCount) {
+                return res.locals.sendResponse({
+                    status: 404,
+                    headers: {},
+                    body: {reason: 'Not Found', message: `${shoe} could not be found.`}
+                });
+            } else {
+                return res.locals.database.query(retrieveTrueToSizeDataForShoeResource, (error, {rows}) => {
+                    if (error) {
+                        return next(error);
+                    } else {
+                        res.locals.shoe = {name: shoe, true_to_sizes: rows.map(({true_to_size}) => true_to_size)};
+                        return next();
+                    }
+                });
+            }
+        });
     }
 };
