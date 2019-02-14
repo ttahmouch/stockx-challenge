971c35409a82a86e2af9286f19d791e03296bcc6
Tony Tahmouch <tony.tahmouch@gm.com>
2019-02-13 23:35:10 -0500

    TT | Updating the table schemas regarding the way the names of shoes are stored.
    + Previously, they were stored with fixed size varchars, and now they are changed to variable size text.

--- database/docker-entrypoint-initdb.d/index.sql
+++ database/docker-entrypoint-initdb.d/index.sql
@@ -1,10 +1,10 @@
 GRANT ALL PRIVILEGES ON DATABASE catalog TO postgres;
 CREATE TABLE shoes (
     id serial PRIMARY KEY,
-    name varchar(100) NOT NULL
+    name text NOT NULL
 );
 CREATE TABLE shoes_true_to_size_data (
     id serial PRIMARY KEY,
-    name varchar(100) NOT NULL,
+    name text NOT NULL,
     true_to_size smallint NOT NULL
 );
