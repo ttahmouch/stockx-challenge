da8a9c750e6a705e02cb0b8e65b65f83b9ff67b5
Tony Tahmouch <tony.tahmouch@gm.com>
2019-02-14 22:01:26 -0500

    TT | Changing the Postgres initialization script to only create tables if they don't already exist.
    + This shouldn't matter right now because none of the database data is persisted between launches of the Docker containers, but it will matter if volumes are used in the future.

--- database/docker-entrypoint-initdb.d/index.sql
+++ database/docker-entrypoint-initdb.d/index.sql
@@ -1,9 +1,9 @@
 GRANT ALL PRIVILEGES ON DATABASE catalog TO postgres;
-CREATE TABLE shoes (
+CREATE TABLE IF NOT EXISTS shoes (
     id serial PRIMARY KEY,
     name text NOT NULL
 );
-CREATE TABLE shoes_true_to_size_data (
+CREATE TABLE IF NOT EXISTS shoes_true_to_size_data (
     id serial PRIMARY KEY,
     name text NOT NULL,
     true_to_size smallint NOT NULL
